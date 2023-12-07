6a1e61320f0be6c02a52df1c71b98b7068f12945
Alex L <alexandre.letellier09@gmail.com>
2023-12-08 00:30:59 +0100

    css-ify quizblock

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -7,32 +7,36 @@ const currentScore = ref(0);
 </script>
 
 <template>
-    <h1>Score: {{ currentScore }}</h1>
-    <QuizBlock @update-score="currentScore += $event"/>    <div class="wrapper">
+    <div class="game-view">
         <div class="left-side inventoryGUI_item">
+            <h1>Score: {{ currentScore }}</h1>
 
         </div>
         <div class="right-side inventoryGUI_item">
-
+            <QuizBlock @update-score="currentScore += $event" />
         </div>
     </div>
 </template>
 
 <style scoped>
+.game-view {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: stretch;
+    height: 100vh;
+}
 
-    .wrapper {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: stretch;
-        height: 100vh;
-    }
+.left-side {
+    flex: 3;
+}
 
-    .left-side {
-        flex: 3;
-    }
-
-    .right-side {
-        flex: 5;
-    }
+.right-side {
+    flex: 5;
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    align-items: stretch;
+    padding: 45px;
+}
 </style>
